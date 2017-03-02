96d5b41c34e1bcf5f2cdaa77d6bb7a7d93fe4674
sakakendo0321 <sakakendo0321@gmail.com>
2017-03-02 22:52:04 +0900

    servoはpfm制御しないといけないらしいから変更した
    なんでこんなにメジャーなものにデータシートもサンプルプログラムも落ちてないんだよ....
    :weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary::weary:

---
+++ servo/08_Servo.c
@@ -0,0 +1,32 @@
+#include "stm32f4xx.h"
+#include "digitalIO.h"
+#include "motor.h"
+#include "timer.h"		// **これが必要
+#include "servo.h"
+
+
+#define __08_SERVO__
+#ifdef __08_SERVO__
+
+//debug用変数　関数に入った回数をカウント
+//irq		servo.c :: TIM2_IRQHandler
+//main_g	main.c :: main()
+//while_g	main.c :: while(1)
+extern int irq;
+int main_g=0,while_g=0;
+
+
+int main(){
+	main_g++;
+ 	SystemCoreClockUpdate();	// **システムクロックを初期化する
+	TIM2_CMT_Init();
+	DIO0_Init(0xFF, DIO_MODE_OUT);	// **モータポートとして使用するピンを出力に設定しておく
+	while_g++;
+	while(1)
+	{
+		Servo_Drive(20,20);
+	}
+	return 0;
+}
+
+#endif //__08_SERVO__
---
+++ servo/servo.c
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *	@ファイル名		:	motor.c
+ *	@概要		:	DIGITAL-IOポートでモータを制御するためのPWM初期化と動作を行う．
+ *					標準のモータドライバを使うことを想定している．
+ *					また，時間待ち関数やシステムタイマも提供している．
+ *	@バージョン		:	1.2.0
+ *	@開発者		:	キネクト
+ *	@使用環境		:	STM32F4DISCOVERY, MB_Ver2, Coocox CoIDE
+ **************************************************************************/
+
+#include <stm32f4xx.h>
+#include "digitalIO.h"
+#include "servo.h"
+#include "lcd.h"		// **これが必要
+#include "timer.h"		// **これが必要
+#include "xprintf.h"
+
+int irq=0;
+
+/* 絶対値を求める */
+//motor.hでincludeされているはずだから一応
+#ifndef ABS_VAL
+#define ABS_VAL(val) ((val) < 0 ? -(val) : (val))
+#endif //ABS_VAL
+
+//debug 用xprintf
+#define printd(fmt,...) xprintf("%s %d %s",__func__,__LINE__,fmt)
+_servo_status_t gServo_status[3]={};
+
+void Servo_Drive(short high,short low){
+	u8 port=0,ch=0;
+	gServo_status[port].high[ch]=high;
+	gServo_status[port].low[ch] =low;
+}
+
+void TIM2_CMT_Init(){
+	//1kHz = 1msでの割込み
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	TIM2->PSC=100-1;
+	TIM2->ARR=840-1;
+	TIM2->CNT=0;
+	TIM2->SR=0;
+    TIM2->DIER=TIM_DIER_UIE;
+	TIM2->CR1|=TIM_CR1_CEN;
+	NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void TIM2_IRQHandler(){
+	int port=0,pin=0;
+	TIM2->SR=0;
+	//static int i=0;
+	//i++;
+/*	for(port=0;port<3;port++){
+		for(pin=0;pin<8;pin++){
+		}
+	}
+*/
+	gServo_status[port].cnt[pin]++;
+	if( 0< gServo_status[port].cnt[pin] && gServo_status[port].cnt[pin] <=gServo_status[port].high[pin]){
+		DIO_OutputPin(port,pin,1);
+	}else if( gServo_status[port].high[pin] <= gServo_status[port].cnt[pin]
+	                 && gServo_status[port].cnt[pin] <=gServo_status[port].high[pin] +gServo_status[port].low[pin] ){
+		DIO_OutputPin(port,pin,0);
+	}else if( 0< gServo_status[port].cnt[pin] && gServo_status[port].cnt[pin] <=gServo_status[port].high[pin]){
+		gServo_status[port].cnt[pin]=0;
+		DIO_OutputPin(port,pin,0);
+	}
+}
