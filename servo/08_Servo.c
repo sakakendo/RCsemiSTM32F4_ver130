219392c38d9fe9ff2e896e4fc08e31818f052947
sakakendo0321 <sakakendo0321@gmail.com>
2017-03-03 10:13:52 +0900

    pfmの生成
    とりあえず不規則だがパルスが立つことを確認
    なぜか不規則...
    pin(0,0)にしか対応していない，またpfm生成関数が不細工なので治す

--- servo/08_Servo.c
+++ servo/08_Servo.c
@@ -1,30 +1,26 @@
 #include "stm32f4xx.h"
 #include "digitalIO.h"
 #include "motor.h"
-#include "timer.h"		// **これが必要
+#include "timer.h"
 #include "servo.h"
 
 
-//#define __08_SERVO__
+#define __08_SERVO__
 #ifdef __08_SERVO__
 
-//debug用変数
-
-//関数に入った回数をカウント
 //irq		servo.c :: TIM2_IRQHandler
 
 //main_g	main.c :: main()
 //while_g	main.c :: while(1)
-//pinの状態をglobalに
 //pin_state	servo.c :: TIM2_IRQHandler
 extern int irq,pin_state;
 int main_g=0,while_g=0;
 
 int main(){
 	main_g++;
- 	SystemCoreClockUpdate();	// **システムクロックを初期化する
+ 	SystemCoreClockUpdate();
 	TIM2_CMT_Init();
-	DIO0_Init(0xFF, DIO_MODE_OUT);	// **モータポートとして使用するピンを出力に設定しておく
+	DIO0_Init(0xFF, DIO_MODE_OUT);
 	
 	while(1)
 	{
--- servo/servo.c
+++ servo/servo.c
@@ -1,23 +1,13 @@
-/***************************************************************************
- *	@ファイル名		:	motor.c
- *	@概要		:	DIGITAL-IOポートでモータを制御するためのPWM初期化と動作を行う．
- *					標準のモータドライバを使うことを想定している．
- *					また，時間待ち関数やシステムタイマも提供している．
- *	@バージョン		:	1.2.0
- *	@開発者		:	キネクト
- *	@使用環境		:	STM32F4DISCOVERY, MB_Ver2, Coocox CoIDE
- **************************************************************************/
 
 #include <stm32f4xx.h>
 #include "digitalIO.h"
 #include "servo.h"
-#include "lcd.h"		// **これが必要
-#include "timer.h"		// **これが必要
+#include "lcd.h"
+#include "timer.h"
 #include "xprintf.h"
 
 int irq=0,pin_state=0;
-/* 絶対値を求める */
-//motor.hでincludeされているはずだから一応
+
 #ifndef ABS_VAL
 #define ABS_VAL(val) ((val) < 0 ? -(val) : (val))
 #endif //ABS_VAL
@@ -33,7 +23,7 @@ void Servo_Drive(short high,short low){
 }
 
 void TIM2_CMT_Init(){
-	//1kHz = 1msでの割込み
+	//1kHz = 1ms
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	TIM2->PSC=100-1;
 	TIM2->ARR=840-1;
@@ -63,7 +53,7 @@ void TIM2_IRQHandler(){
 	                 && gServo_status[port].cnt[pin] <=gServo_status[port].high[pin] +gServo_status[port].low[pin] ){
 		DIO_OutputPin(port,pin,0);
 		pin_state=0;
-	}else if( 0< gServo_status[port].cnt[pin] && gServo_status[port].cnt[pin] <=gServo_status[port].high[pin]){
+	}else if( gServo_status[port].high[pin] +gServo_status[port].low[pin] <= gServo_status[port].cnt[pin]){
 		gServo_status[port].cnt[pin]=0;
 		DIO_OutputPin(port,pin,0);
 		pin_state=0;
